cb9daa58822e37a1eff615aa76b05181ed4c6952
Cecilia-04 <c.banda@alustudent.com>
2022-07-08 08:53:36 -0700

    Infinite addition

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+
+def min():
+    args = sys.argv[1:]
+    sum = 0
+    for arg in args:
+        sum += int(arg)
+    print(sum)
+
+
+if _main_ == "_main_":
+    main()
