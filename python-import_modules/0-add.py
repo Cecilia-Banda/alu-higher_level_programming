e2af7662ff5abdca58bc2eccb3df5e7c09d9fe6c
Cecilia-04 <c.banda@alustudent.com>
2022-07-08 08:39:05 -0700

    Import a simple function from a simple file

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from add_0 import add
+
+
+def main():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if _name_ == "_main_":
+    main
---
+++ python-import_modules/README.md
@@ -0,0 +1,1 @@
+This contains import and modules data
