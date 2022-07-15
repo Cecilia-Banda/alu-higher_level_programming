8c4f79c83375e03644c760457f12b4c4c431fbfb
Cecilia-04 <c.banda@alustudent.com>
2022-07-15 09:30:37 -0700

    Can you Cee me?

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,18 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-
-     convertList = list(my_string)
-    index = 0
-    editedString = ""
-
-    if (len(my_string)) == 0:
-        return my_string
-
-    new_string = convertList[:]
-
-    for elements in new_string[:]:
-        if elements == 'c' or elements == 'C':
-            del(new_string[index])
-            index -= 1
-        index += 1
-    return (editedString.join(new_string))
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
