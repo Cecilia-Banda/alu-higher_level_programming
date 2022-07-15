36852aa52c6140783ccf74b8613ac58d87a5eb1f
Cecilia-04 <c.banda@alustudent.com>
2022-07-15 04:48:35 -0700

    Secure access

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+
+    index = 0
+    if idx < 0:
+        return None
+
+    if idx > index:
+        return None
+
+    for element in my_list:
+        if index == idx:
+            return (element)
+        index += 1
