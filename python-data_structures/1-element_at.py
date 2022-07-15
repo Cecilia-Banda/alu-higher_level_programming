251ee7416834dca50aca6b2c4e09956ba696e28f
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-07-15 20:53:34 +0200

    Update 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -5,10 +5,9 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
 
-    if idx > index:
-        return None
-
     for element in my_list:
         if index == idx:
             return (element)
         index += 1
+    if idx > index:
+        return None
