24077b4b60b7bbff1d537098a64065fed2efb538
Cecilia-04 <c.banda@alustudent.com>
2022-07-15 09:38:08 -0700

    Beaucoup Returns!

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = None
+    if(length > 0):
+        first = sentence[0]
+    return lenght, first    
+    
