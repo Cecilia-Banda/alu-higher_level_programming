3183d46a34b04c358b99a600ea2d16de4d0086cd
Cecilia-04 <c.banda@alustudent.com>
2022-07-21 11:00:06 -0700

    Safe list printing

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    index = 0
+
+    while index < x:
+        try:
+            print("{}".format(my_list[index]), end=" ")
+            index +=1
+        except IndexError:
+            break
+     print()
+     return(index)
---
+++ python-exceptions/README.md
@@ -0,0 +1,1 @@
+This includes data under exceptions
