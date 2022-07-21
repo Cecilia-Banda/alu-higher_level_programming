af59792c7f698e0ce1495da46d83bc85761d2d0e
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-07-21 20:07:49 +0200

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
 
     while index < x:
         try:
-            print("{}".format(my_list[index]), end=" ")
+            print("{}".format(my_list[index]), end="")
             index +=1
         except IndexError:
             break
