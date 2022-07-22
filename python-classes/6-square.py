317bcc6f084a10f32d64f75981025524b9477fce
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-07-22 20:18:08 +0200

    Update 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Define a class square."""
+"""Define a class Square."""
 
 
-class square:
+class Square:
     """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Begin a new square.
+        """Initialize a new square.
         Args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
@@ -16,7 +16,7 @@ class square:
 
     @property
     def size(self):
-        """Prompt the current size of the square."""
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
