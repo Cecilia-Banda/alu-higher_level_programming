7e239b197c30cf3c74bac09c323c214f0759ad87
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-07-22 23:06:22 +0200

    Update 5-rectangle.py

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -1,2 +1,63 @@
 #!/usr/bin/python3
 """Comprises class Rectangle"""
+
+
+class Rectangle:
+    """describe a rectangle instance
+     It contains width, height attribute, area, and perimeter
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        printable = ""
+        if self.__height == 0 or self.__width == 0:
+            return printable
+        for i in range(self.__height):
+            if i == self.__height - 1:
+                printable += "#" * self.__width
+                break
+            printable += "#" * self.__width + "\n"
+        return printable
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        return print("Bye rectangle...")
