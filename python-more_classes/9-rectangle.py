4b65e639088f05c179df3b1c729d64dfc147bb54
Cecilia-04 <c.banda@alustudent.com>
2022-07-22 13:24:04 -0700

    A square is a rectangle

---
+++ python-more_classes/9-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Contaning class Rectangle"""
+
+
+class Rectangle:
+    """describe a rectangle instance
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+This folder is stritly for information concerning more classes
