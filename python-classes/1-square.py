698048e72cd41dc4bff74ba491da743bc487f392
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-07-22 19:43:21 +0200

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,9 +6,9 @@
 class Square:
     """Represent a square."""
 
-    def _init_(self,size):
-        """Initialie a new Square.
+    def __init__(self, size):
+        """Initialize a new Square.
         Args:
-             size (int): The size of the new square.
+            size (int): The size of the new square.
         """
-        self._size = size
+        self.__size = size
