d9fbc40ef61a5b1b610e835c748cd54ef04397ed
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-07-22 17:35:22 +0200

    Update 3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
- try:
-        c = a/b
+    result = None
+    try:
+        result = a / b
     except ZeroDivisionError:
-        c = None
+        result = None
     finally:
-        print("Inside result: {}".format(c))
-    return (c)
+        print("Inside result: {}".format(result))
+        return result
