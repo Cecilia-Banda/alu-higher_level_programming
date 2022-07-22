e181142547d50fdc4154ea6122601b29d0360a79
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-07-22 22:46:22 +0200

    Update 1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,2 +1,36 @@
 #!/usr/bin/python3
 """Consists class Rectangle"""
+
+
+class Rectangle:
+    """explain a rectangle with height and width assigned"""
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
