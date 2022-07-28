732bde29803127e503e4edf1d0445b093cd348f4
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-07-28 14:54:31 +0200

    Update 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/binpython3
+"""Defines an object's characteristic lookup function."""
+
+
 def lookup(obj):
-    returns list
+    """Return a list of the object's available Characteristic."""
+    return (dir(obj))
