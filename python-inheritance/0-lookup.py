a6a3da5bb008f187c898161018899fae85827e84
Cecilia-04 <c.banda@alustudent.com>
2022-07-29 07:06:45 -0700

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,11 @@
-#!/usr/binpython3
-"""Defines an object's characteristic lookup function."""
+#!/usr/bin/python3
+"""list of available attributes and methods of an object"""
 
 
 def lookup(obj):
-    """Return a list of the object's available Characteristic."""
-    return (dir(obj))
+    """Return attributes
+    Args :
+        - obj: object to get attributes
+    """
+
+    return dir(obj)
