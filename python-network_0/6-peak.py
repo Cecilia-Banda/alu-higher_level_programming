5d6da486e156da8f76eb71ad4c8192520e7df239
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-07-29 13:19:37 +0200

    Update 6-peak.py

--- python-network_0/6-peak.py
+++ python-network_0/6-peak.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  Function that finds a peak in a list of unsorted integers. """
+""" Function that finds a peak in a list of unsorted integers. """
 
 
 def find_peak(list_of_integers):
