2973ca00d919ab3494d807a63df8b001a1c7735c
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-07-29 16:28:42 +0200

    Update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    my_tuple = ()
-    if len(sentence) == 0:
-        my_tuple = 0, "None"
+    if sentence:
+        sentence_length = len(sentence)
     else:
-        my_tuple = len(sentence), sentence[0]
-    return
+        sentence_length = 0
+    return tuple((sentence_length, None if not sentence else sentence[:1]))
