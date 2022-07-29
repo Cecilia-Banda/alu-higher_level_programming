d00dd1eb497772e1bed56abe31682d52ed135b6e
Cecilia-04 <c.banda@alustudent.com>
2022-07-29 04:08:16 -0700

    Find a peak

---
+++ python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" finding a peak in a list of unsorted integers. """
+
+
+def find_peak(list_of_integers):
+    """ Sort the list """
+
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
