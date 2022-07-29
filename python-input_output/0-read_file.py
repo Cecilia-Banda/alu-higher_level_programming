2fbd9c5e02f08912cbe639ce00d265586e42687d
Cecilia-04 <c.banda@alustudent.com>
2022-07-29 04:50:11 -0700

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def read_file(filename=""):
+"""rread file"""
 
+
+def read_file(filename=""):
+    """read file"""
+    with open(filename) as f:
+        line = f.read()
+        print(line, end="")
