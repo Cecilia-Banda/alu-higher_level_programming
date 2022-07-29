2d4bdd5578d3793e0ffe02db4bd30b6b896b1a56
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-07-29 12:18:27 +0200

    Update 2-delete.sh

--- python-network_0/2-delete.sh
+++ python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script sending a DELETE request to the URL passed on first arguement and displays the body of the response
-curl -sI "$1" | grep "Allow:" | cut -d " " -f2-
+curl -sX DELETE "$1"
