524df78f03b370da39cc39f13f67c98fdea4a10b
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-08-03 14:52:50 +0200

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script takes in a URL and an email, sends a POST request to the passed URL with the
+takes in a URL and an email, sends a POST request to the passed URL with the
 email as a parameter, and displays the body of the response
 """
 if __name__ == "__main__":
