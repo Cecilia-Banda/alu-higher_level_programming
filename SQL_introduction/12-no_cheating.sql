9ab0c3cf2fe010db1f9e0645844b71d19d1d9f3e
Cecilia-04 <c.banda@alustudent.com>
2022-08-05 07:49:45 -0700

    Cheating is bad

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- script that updates Bob's score to 10 in the second_table table --
+-- script that updates Bob's score to 10 in the second_table table --
+UPDATE second_table SET score = 10 WHERE name = "Bob";
