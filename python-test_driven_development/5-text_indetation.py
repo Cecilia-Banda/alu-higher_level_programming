51126c6980378a79aac2e66cee62e299f900342e
Cecilia-04 <105134294+Cecilia-04@users.noreply.github.com>
2022-08-18 19:29:51 +0200

    Update 5-text_indetation.py

--- python-test_driven_development/5-text_indetation.py
+++ python-test_driven_development/5-text_indetation.py
@@ -6,7 +6,7 @@ Module 5-text_indentation
 
 def text_indentation(text):
     """
-    Prints text with 2 new lines after each ".", and ":"
+    Prints text with 2 new lines after each ".", "?", and ":"
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
